c2f74762b6605b0651846daac8970a9a1a55c602
Maxim Lamare <maxim.lamare@meteo.fr>
2020-01-15 18:51:58 +0100

    Move functions to toolbox

--- processing_tools/proc_toolbox.py
+++ processing_tools/proc_toolbox.py
@@ -148,3 +148,56 @@ def open_large_raster(inpath):
     band = None
 
     return array, gt, ds
+
+
+def array_to_raster(array, prj, gt, dst_filename):
+    """Array > Raster
+    Save a raster from a C order array.
+
+    :param array: ndarray
+    """
+    driver = gdal.GetDriverByName('GTiff')
+
+    dataset = driver.Create(
+        dst_filename,
+        array.shape[1],
+        array.shape[0],
+        1,
+        gdal.GDT_Float32,)
+
+    dataset.SetGeoTransform(gt)
+
+    dataset.SetProjection(prj)
+    dataset.GetRasterBand(1).WriteArray(array)
+    dataset.GetRasterBand(1).SetNoDataValue(-9999)
+    dataset.FlushCache()  # Write to disk.
+
+
+def blockshaped(arr, nrows, ncols):
+    """
+    Return an array of shape (n, nrows, ncols) where
+    n * nrows * ncols = arr.size
+
+    If arr is a 2D array, the returned array should look like n subblocks with
+    each subblock preserving the "physical" layout of arr.
+    """
+    h, w = arr.shape
+    assert h % nrows == 0, "{} rows is not evenly divisble by {}".format(h, nrows)
+    assert w % ncols == 0, "{} cols is not evenly divisble by {}".format(w, ncols)
+    return (arr.reshape(h//nrows, nrows, -1, ncols)
+               .swapaxes(1, 2)
+               .reshape(-1, nrows, ncols))
+
+
+def unblockshaped(arr, h, w):
+    """
+    Return an array of shape (h, w) where
+    h * w = arr.size
+
+    If arr is of shape (n, nrows, ncols), n sublocks of shape (nrows, ncols),
+    then the returned array preserves the "physical" layout of the sublocks.
+    """
+    n, nrows, ncols = arr.shape
+    return (arr.reshape(h//nrows, -1, nrows, ncols)
+               .swapaxes(1, 2)
+               .reshape(h, w))
